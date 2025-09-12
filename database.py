292630f3bc05b3dfe1e1d16bca76dd24e51c38be
valeron <valeronchik0702@gmail.com>
2025-09-12 09:22:30 +0300

    added parsing env file

--- database.py
+++ database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from decouple import config
 
-SQLALCHEMY_DATABASE_URL = "postgresql://valeron:rfk,fcf@172.23.1.3/resume"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config('DATABASE_USER')}:{config('DATABASE_PASSWORD')}@172.23.1.3/{config('DATABASE_NAME')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- test.py
+++ test.py
@@ -8,19 +8,19 @@ from src.schema import UserCreate, UserLogin, Token
 from depencies import get_db
 import bcrypt
 
-SQLALCHEMY_DATABASE_URL = "postgresql://valeron:rfk,fcf@172.23.1.3/resume"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config('DATABASE_USER')}:{config('DATABASE_PASSWORD')}@172.23.1.3/{config('DATABASE_NAME')}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
-db = SessionLocal()
+# SessionLocal = sessionmaker(autoflush=False, bind=engine)
+# db = SessionLocal()
 
-Base = declarative_base()
+# Base = declarative_base()
 
-login = "valeron@gmail.com"
+# login = "valeron@gmail.com"
 
-users = db.query(User).all()
-print(users)
+# users = db.query(User).all()
+# print(users)
 
 
 # pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -28,5 +28,20 @@ print(users)
 
 
 # eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxIiwiZXhwIjoxNzU3Mjc3NTA4fQ.Ww7PvXmavn_rW-ozU2bVOu06iGbtqEIBodrjXWFfdi0
+
+import os
+from os.path import join
+
+
+# base_dir = os.path.dirname(os.path.abspath(__file__))
+
+# env_config = config(RepositoryEnv(join(base_dir, '.env')))
+
+# user = env_config.get('DATABASE_USER')
+
+user = config('DATABASE_USER')
+
+print(SQLALCHEMY_DATABASE_URL)
+
 
 
